4b07f3fa5af34adb76f0524a53ef2a30b3d2543d
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2025-04-15 17:30:05 -0400

    V0.1 (Stelle)
    10/10 visual novel

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Image, SafeAreaView, Button, Alert } from 'react-native';
 
 export default function App() {
+  console.log("all good aha~"); // DELETE THIS WHEN DONE
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <Text> What Will You Do?</Text>
+      <SafeAreaView style={styles.button_container}>
+        <Button 
+        title="High Five" 
+        onPress={() => Alert.alert("YAY!")}/>
+
+        <Button 
+        title="STAB!"
+        color="red"
+        onPress={() => Alert.alert("you monster...")}/>
+
+      </SafeAreaView>
+
+      <TouchableWithoutFeedback>
+        <Image source={{ 
+          width: 350,
+          height: 550,
+          uri: "https://static.wikia.nocookie.net/shipping/images/1/1b/Character_Trailblazer_29_Destruction_Portrait.png/revision/latest?cb=20230716035459"}}/>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff', // #fffacd
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  button_container: {
+    alignItems: 'center',
   },
 });
