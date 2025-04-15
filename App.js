cfd535dc83b1a7cd89123c413ef25aa21df572ea
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2025-04-15 19:32:05 -0400

    V0.3 (WORKING NAVBAR)
    the nav bar works (not the links but who cares)

--- App.js
+++ App.js
@@ -1,47 +1,147 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableWithoutFeedback, Image, SafeAreaView, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Image, SafeAreaView, Button, Alert, ScrollView } from 'react-native';
 
 export default function App() {
   console.log("all good aha~"); // DELETE THIS WHEN DONE
 
   return (
-    <SafeAreaView style={styles.container}>
-      <SafeAreaView style={styles.navbar}>
-        <Text style={styles.navbar_text}>Examples</Text>
-        <Text style={styles.navbar_text}>Prices</Text>
-        <TouchableWithoutFeedback>
-          <Image source={{ 
-            width: 50,
-            height: 50,
-            uri: "https://github.com/MlgEpicCar/LemonAID/blob/main/images/LemonLogo.png?raw=true"}}/>
-        </TouchableWithoutFeedback>
-        <Text style={styles.navbar_text}>Team</Text>
-        <Text style={styles.navbar_text}>Account</Text>
+    <View>
+      <SafeAreaView style={styles.container}>
+        <SafeAreaView style={styles.navbar}>
+          
+          
+          <View style={styles.navbar_view}>
+            <Text style={styles.navbar_text}>Projects</Text>
+          </View>
+          <View style={styles.navbar_view}>
+            <Text style={styles.navbar_text}>Prices</Text>
+          </View>
+          <TouchableWithoutFeedback style={styles.navbar_view}>
+            <Image source={{ 
+              width: 70,
+              height: 70,
+              uri: "https://github.com/MlgEpicCar/LemonAID/blob/main/images/LemonLogo.png?raw=true"}}/>
+          </TouchableWithoutFeedback>
+          <View style={styles.navbar_view}>
+            <Text style={styles.navbar_text}>Team</Text>
+          </View>
+          <View style={styles.navbar_view}>
+            <Text style={styles.navbar_text}>Account</Text>
+          </View>
 
+        </SafeAreaView>
       </SafeAreaView>
-    </SafeAreaView>
+
+
+      <ScrollView style={body_styles.container}>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+        <Text> hello</Text>
+
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 0,
-    backgroundColor: '#fffacd',
+    backgroundColor: '#ffe761',
   },
 
   navbar: { // Styles for navagation bar
-    backgroundColor: '#fffacd',
+    backgroundColor: '#ffe761',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
-    marginBottom: 10,
+    margin: (0, 6),
   },
 
   navbar_text: {
-    margin: (10, 10, 10, 10),
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
   },
 
-  body: {
-    backgroundColor: 'ffffff'
-  }
+  navbar_view: {
+    justifyContent: 'center',
+    flex: 1,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#ffd761',
+    margin: 3,
+    borderColor: '#e9b424',
+    borderWidth: 1,
+  },
+});
+
+const body_styles = StyleSheet.create({
+  container: {
+    flex: 0, // I have no clue as to why, but if you set the flex to 1 everything with this style WILL disappear
+    backgroundColor: '#fffacd',
+  },
 });
